76b6693a6f4cb651d38180baf8aa19700a0885d7
ktgross15 <ktgross15>
2020-04-17 11:24:11 -0400

    Updated recipe CREDITCARDFRAUDKRG.compute_unseen_python

--- recipes/compute_unseen_python.py
+++ recipes/compute_unseen_python.py
@@ -5,15 +5,9 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 unseen_scored = dataiku.Dataset("unseen_scored")
-unseen_scored_df = unseen_scored.get_dataframe()
+df = unseen_scored.get_dataframe()
 
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-unseen_python_df = unseen_scored_df # For this sample code, simply copy input to output
-
+df['new'] = 'new stuff yay'
 
 # Write recipe outputs
 unseen_python = dataiku.Dataset("unseen_python")
